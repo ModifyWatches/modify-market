d5f6b4a50b346c135c224f633a7833d58600f2e4
Modify Tech <tech@modifywatches.com>
2016-02-28 16:27:37 -0800

    more stuff
    more stuff

--- assets/follow.js
+++ assets/follow.js
@@ -2,13 +2,15 @@ $(document).ready(function() {
 	$('#follower').click(function() {
 		if($('#EMAIL').val() !='') {
 			data = $('#followers').serialize();
+			$('#EMAIL').hide();
+			$('#follower').text('following').prop( "disabled", true );
+			$('#followText').html('You are now following '+$('#collection').val());
 			$.ajax({
 			  type: "POST",
 			  url: 'http://dev.modifywatches.com/sadhorse/followers.php',
 			  data: data,
 			  success: function(result) {
 			  		console.log(result);
-			  		$('#EMAIL').hide();
 			  }
 			});
 		}else {
