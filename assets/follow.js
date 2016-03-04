6e6a10f9cb695185b028cf91340f414723ab5ed0
Modify Tech <tech@modifywatches.com>
2016-03-03 16:36:33 -0800

    following final

--- assets/follow.js
+++ assets/follow.js
@@ -1,11 +1,25 @@
 	$(document).ready(function() {
-	$('#follower').click(function() {
+		var theCollection = $('#collection').val();
+		if (document.cookie.indexOf(theCollection) >= 0) {
+			$('#EMAIL').hide();
+			$('#follower').text('following').prop( "disabled", true );
+			
+		}
+	$('#follower').click(function(e) {
+		e.preventDefault();
+		var totalFollowers = parseInt($('#numFollowers').val());
+		var CookieDate = new Date;
+		CookieDate.setFullYear(CookieDate.getFullYear( ) +10);
+		var theCookie = $('#collection').val(); 
 		if($('#EMAIL').val() !='') {
+			totalFollowers++;
 			data = $('#followers').serialize();
 			$('#EMAIL').hide();
 			$('#follower').text('following').prop( "disabled", true );
 			$('#followText').html("You’re now first in line to see all of "+$('#collection').val()+"'s new products!");
 			$('#followers').submit();
+			$('#followText').html(totalFollowers+' Fans');
+			document.cookie = theCookie+'=yes; expires=' + CookieDate.toGMTString( ) + '; path=/';
 			
 		}else {
 			$('#EMAIL').addClass('required');
