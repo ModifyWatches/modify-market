0c9229cce06368c479d2aae24b239e29ca1280f1
Modify Tech <tech@modifywatches.com>
2016-04-25 13:55:06 -0700

    more following bugs

--- assets/follow.js
+++ assets/follow.js
@@ -28,5 +28,30 @@
 		}
 	
 	})
+
+	$('#follower').click(function(e) {
+		 
+		var totalFollowers = parseInt($('#numFollowers').val());
+		var CookieDate = new Date;
+		CookieDate.setFullYear(CookieDate.getFullYear( ) +10);
+		var theCookie = $('#collection').val(); 
+		if($('#EMAIL').val() !='') {
+			totalFollowers++;
+			data = $('#followers').serialize();
+			$('#EMAIL').hide();
+			$('#follower').text('following').prop( "disabled", true );
+			$('#followText').html("You’re now first in line to see all of "+$('#collection').val()+"'s new products!");
+			
+			$('#followText').html(totalFollowers+' Fans');
+			document.cookie = theCookie+'=yes; expires=' + CookieDate.toGMTString( ) + '; path=/';
+			$('#followers').submit();
+			
+		}else {
+			e.preventDefault();
+			$('#EMAIL').addClass('required');
+		}
+	
+	})
+
 	
 })
